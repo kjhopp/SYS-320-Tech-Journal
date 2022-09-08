0928490187cca60f301f6f71c0658d9197958081
kjhopp <56058382+kjhopp@users.noreply.github.com>
2022-09-08 16:31:56 -0400

    Add files via upload

---
+++ Week01/class/syslogCheck.py
@@ -0,0 +1,40 @@
+# Create an interface to search through syslog files
+import re, sys
+
+def _syslog(filename,listOfKeywords):
+
+    # Open a file
+    with open(filename) as f:
+
+        # read in the file nd save it to a variable
+        contents = f.readlines()
+
+    # Lists to store the results -- outside the loop
+    results = []
+
+    # Loop through the list returned. Each element is a line from the smallSyslog file
+    for line in contents:
+        
+        # Loops through the keywords
+        for eachKeyword in listOfKeywords:
+
+            # If the line contains the keyword then it will print
+            # if eachKeyword in line:
+            # Searches and returns result using a regular expression search
+            x = re.findall(r''+eachKeyword+'', line)
+
+            for found in x:
+
+                # Append the returned keywords to the results list
+                results.append(found)
+
+    # Check to see if there are results
+    if len(results) == 0:
+        print("No Results")
+        sys.exit(1)
+    
+    # Sort the list
+    results = sorted(results)
+
+    return results
+    # print(x)
---
+++ Week01/class/syslogSSH.py
@@ -0,0 +1,31 @@
+import syslogCheck
+import importlib
+importlib.reload(syslogCheck)
+
+# SSH authentication failures
+def ssh_fail(filename, searchTerms):
+
+    # Call syslogCheck and return the results
+    is_found = syslogCheck._syslog(filename,searchTerms)
+
+    #  Found List
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+        
+        # print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        found.append(sp_results[8])
+    
+    # Remove duplicates using set
+    # and concvert the lis tot a dictionary
+    hosts = set(found)
+
+    # print results
+    for eachHost in hosts:
+
+        print(eachHost)
