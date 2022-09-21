1a53848f5a43cb7eb3dd9079712aa8b834831c88
kjhopp <56058382+kjhopp@users.noreply.github.com>
2022-09-21 11:25:34 -0400

    Add files via upload

---
+++ Week03/homework/urlHaus.py
@@ -0,0 +1,36 @@
+import csv
+# imported re module -- included in the code below
+import re
+
+# put in the correct indentations
+
+# changed function from ur1HausOpen to urlHausOpen
+# changed searchTerm to correct searchTerms
+def urlHausOpen(filename,searchTerms):
+# removed quotes on filename
+# changed to with
+    with open(filename) as f:
+    # changed from == to =
+        contents = csv.reader(f)
+        
+        for _ in range(9):
+            next(contents)
+    
+    # loop was not in the right order
+        for eachLine in contents:
+
+            for keyword in searchTerms:
+                # added quotations accordingly
+                x = re.findall(r'' + keyword + '', eachLine[2])
+
+
+            for _ in x:
+# Don't edit this line. It is here to show how it is possible
+# to remove the "tt" so programs don't convert the malicious
+# domains to links that an be accidentally clicked on.
+                the_url = eachLine[2].replace("http","hxxp")
+                the_src = eachLine[7]
+                print("""
+                    URL: %s
+                    Info: %s 
+                    %s """ % (the_url, the_src,"~"*60)) 
